e728200203f80805114eec73177386a5b3cdcbfb
Юрий Троянов <troyan0v@yandex.ru>
2022-08-31 23:33:48 +0300

    после успешного pytest

--- homework.py
+++ homework.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import requests
 import time
@@ -5,8 +6,31 @@ import telegram
 
 from dotenv import load_dotenv
 
-load_dotenv()
+from exceptions import (
+    NoKeysException,
+    FailSendException,
+    DisableEndpointException,
+    ProblemEndpointException,
+    ProcessingProblemException,
+    SurpriseStatusException
+)
 
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename='bot.log',
+    format='%(asctime)s, %(levelname)s, %(message)s',
+    filemode='w'
+)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+hendler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s, %(levelname)s, %(message)s')
+hendler.setFormatter(formatter)
+logger.addHandler(hendler)
+
+
+load_dotenv()
 
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
@@ -27,19 +51,31 @@ LAST_STATUS = ''
 
 
 def send_message(bot, message):
-    bot.send_message(TELEGRAM_CHAT_ID, message)
+    try:
+        bot.send_message(TELEGRAM_CHAT_ID, message)
+        logger.info('Сообщение успешно отправлено.')
+    except:
+        raise FailSendException
 
 
 def get_api_answer(current_timestamp):
-    timestamp = current_timestamp #or int(time.time())
+    timestamp = current_timestamp
     params = {'from_date': timestamp}
     homework_statuses = requests.get(ENDPOINT, headers=HEADERS, params=params)
+    if homework_statuses.status_code != 200:
+        raise DisableEndpointException
     return homework_statuses.json()
 
 
 def check_response(response):
-    if response.get('code') == None:
-        return response.get('homeworks')
+    if type(response) != dict and type(response['homeworks']) != dict:
+        raise ProblemEndpointException
+    if 'homeworks' not in response:
+        raise ProcessingProblemException
+    if type(response.get('homeworks')) != list:
+        raise ProblemEndpointException
+    return response.get('homeworks')
+    
 
 
 def parse_status(homework):
@@ -48,11 +84,10 @@ def parse_status(homework):
     homework_status = homework.get('status')
     verdict = HOMEWORK_STATUSES[homework_status]
     if LAST_STATUS == verdict:
-        pass
-    
-    LAST_STATUS = verdict
-
-    return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+        logger.debug('В ответе отсутствуют новые статусы.')
+    else:
+        LAST_STATUS = verdict
+        return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def check_tokens():
@@ -64,9 +99,11 @@ def check_tokens():
 def main():
     """Основная логика работы бота."""
 
+    if check_tokens() == False:
+        raise NoKeysException
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = 0#int(time.time())
-
+    current_timestamp = 0
+    LAST_MESSAGE = ''
 
     while True:
         try:
@@ -74,14 +111,57 @@ def main():
             homeworks = check_response(response)
             message = parse_status(homeworks[0])
             send_message(bot, message)
+            current_timestamp = int(time.time())
             time.sleep(RETRY_TIME)
-
+        except NoKeysException as error:
+            message = NoKeysException.__doc__
+            logger.critical(message)
+            if LAST_MESSAGE != message:
+                bot.send_message(TELEGRAM_CHAT_ID, message)
+                LAST_MESSAGE = message
+            time.sleep(RETRY_TIME)
+        except FailSendException as error:
+            message = FailSendException.__doc__
+            logger.error(message)
+            if LAST_MESSAGE != message:
+                bot.send_message(TELEGRAM_CHAT_ID, message)
+                LAST_MESSAGE = message
+            time.sleep(RETRY_TIME)
+        except DisableEndpointException as error:
+            message = DisableEndpointException.__doc__
+            logger.error(message)
+            if LAST_MESSAGE != message:
+                bot.send_message(TELEGRAM_CHAT_ID, message)
+                LAST_MESSAGE = message
+            time.sleep(RETRY_TIME)
+        except ProblemEndpointException as error:
+            message = ProblemEndpointException.__doc__
+            logger.error(message)
+            if LAST_MESSAGE != message:
+                bot.send_message(TELEGRAM_CHAT_ID, message)
+                LAST_MESSAGE = message
+            time.sleep(RETRY_TIME)
+        except ProcessingProblemException as error:
+            message = ProcessingProblemException.__doc__
+            logger.error(message)
+            if LAST_MESSAGE != message:
+                bot.send_message(TELEGRAM_CHAT_ID, message)
+                LAST_MESSAGE = message
+            time.sleep(RETRY_TIME)
+        except SurpriseStatusException as error:
+            message = SurpriseStatusException.__doc__
+            logger.error(message)
+            if LAST_MESSAGE != message:
+                bot.send_message(TELEGRAM_CHAT_ID, message)
+                LAST_MESSAGE = message
+            time.sleep(RETRY_TIME)
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
-            ...
+            logger.error(message)
+            if LAST_MESSAGE != message:
+                bot.send_message(TELEGRAM_CHAT_ID, message)
+                LAST_MESSAGE = message
             time.sleep(RETRY_TIME)
-        else:
-            ...
 
 
 if __name__ == '__main__':
