0248839dee46c88755add922b0b0a4ba3e408d1e
Юрий Троянов <troyan0v@yandex.ru>
2022-09-01 00:08:18 +0300

    2 попытка отправки

--- exceptions.py
+++ exceptions.py
@@ -1,28 +1,34 @@
 class NoKeysException(Exception):
     """Отсутствует элемент переменного окружения."""
+
     pass
 
 
 class FailSendException(Exception):
     """Сбой отправки сообщения в телеграмм."""
+
     pass
 
 
 class DisableEndpointException(Exception):
     """Указанный эндпоинт недоступен."""
+
     pass
 
 
 class ProblemEndpointException(Exception):
     """Сбой в обращении к эндпоинту."""
+
     pass
 
 
 class ProcessingProblemException(Exception):
     """Отсутствие ожидаемых ключей в ответе API."""
+
     pass
 
 
 class SurpriseStatusException(Exception):
     """Обнаружен недокументированный статус работы."""
+
     pass
--- homework.py
+++ homework.py
@@ -48,17 +48,20 @@ HOMEWORK_STATUSES = {
 }
 
 LAST_STATUS = ''
+LAST_MESSAGE = ''
 
 
 def send_message(bot, message):
+    """Отправка сообщения ботом."""
     try:
         bot.send_message(TELEGRAM_CHAT_ID, message)
         logger.info('Сообщение успешно отправлено.')
-    except:
-        raise FailSendException
+    except FailSendException:
+        logger.error(FailSendException.__doc__)
 
 
 def get_api_answer(current_timestamp):
+    """Получения ответа от API."""
     timestamp = current_timestamp
     params = {'from_date': timestamp}
     homework_statuses = requests.get(ENDPOINT, headers=HEADERS, params=params)
@@ -68,6 +71,7 @@ def get_api_answer(current_timestamp):
 
 
 def check_response(response):
+    """Проверка ответа от API."""
     if type(response) != dict and type(response['homeworks']) != dict:
         raise ProblemEndpointException
     if 'homeworks' not in response:
@@ -75,10 +79,10 @@ def check_response(response):
     if type(response.get('homeworks')) != list:
         raise ProblemEndpointException
     return response.get('homeworks')
-    
 
 
 def parse_status(homework):
+    """Обработка ответа и вывод статуса работы."""
     global LAST_STATUS
     homework_name = homework.get('homework_name')
     homework_status = homework.get('status')
@@ -91,19 +95,34 @@ def parse_status(homework):
 
 
 def check_tokens():
-    return (TELEGRAM_TOKEN != None
-           and TELEGRAM_CHAT_ID != None
-           and PRACTICUM_TOKEN != None)
+    """Проверка наличия переменных окружения."""
+    return (TELEGRAM_TOKEN is not None
+            and TELEGRAM_CHAT_ID is not None
+            and PRACTICUM_TOKEN is not None)
 
 
+def except_return(bot, exception_name):
+    global LAST_MESSAGE
+    if exception_name == Exception:
+        message = f'Сбой в работе программы: {exception_name}'
+    message = exception_name.__doc__
+    if exception_name == NoKeysException:
+        logger.critical(message)
+    else:
+        logger.error(message)
+    if LAST_MESSAGE != message:
+        bot.send_message(TELEGRAM_CHAT_ID, message)
+        LAST_MESSAGE = message
+    time.sleep(RETRY_TIME)
+
+
+# flake8: noqa: C901
 def main():
     """Основная логика работы бота."""
-
-    if check_tokens() == False:
+    if not check_tokens():
         raise NoKeysException
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = 0
-    LAST_MESSAGE = ''
 
     while True:
         try:
@@ -113,55 +132,20 @@ def main():
             send_message(bot, message)
             current_timestamp = int(time.time())
             time.sleep(RETRY_TIME)
-        except NoKeysException as error:
-            message = NoKeysException.__doc__
-            logger.critical(message)
-            if LAST_MESSAGE != message:
-                bot.send_message(TELEGRAM_CHAT_ID, message)
-                LAST_MESSAGE = message
-            time.sleep(RETRY_TIME)
-        except FailSendException as error:
-            message = FailSendException.__doc__
-            logger.error(message)
-            if LAST_MESSAGE != message:
-                bot.send_message(TELEGRAM_CHAT_ID, message)
-                LAST_MESSAGE = message
-            time.sleep(RETRY_TIME)
-        except DisableEndpointException as error:
-            message = DisableEndpointException.__doc__
-            logger.error(message)
-            if LAST_MESSAGE != message:
-                bot.send_message(TELEGRAM_CHAT_ID, message)
-                LAST_MESSAGE = message
-            time.sleep(RETRY_TIME)
-        except ProblemEndpointException as error:
-            message = ProblemEndpointException.__doc__
-            logger.error(message)
-            if LAST_MESSAGE != message:
-                bot.send_message(TELEGRAM_CHAT_ID, message)
-                LAST_MESSAGE = message
-            time.sleep(RETRY_TIME)
-        except ProcessingProblemException as error:
-            message = ProcessingProblemException.__doc__
-            logger.error(message)
-            if LAST_MESSAGE != message:
-                bot.send_message(TELEGRAM_CHAT_ID, message)
-                LAST_MESSAGE = message
-            time.sleep(RETRY_TIME)
-        except SurpriseStatusException as error:
-            message = SurpriseStatusException.__doc__
-            logger.error(message)
-            if LAST_MESSAGE != message:
-                bot.send_message(TELEGRAM_CHAT_ID, message)
-                LAST_MESSAGE = message
-            time.sleep(RETRY_TIME)
-        except Exception as error:
-            message = f'Сбой в работе программы: {error}'
-            logger.error(message)
-            if LAST_MESSAGE != message:
-                bot.send_message(TELEGRAM_CHAT_ID, message)
-                LAST_MESSAGE = message
-            time.sleep(RETRY_TIME)
+        except NoKeysException:
+            except_return(bot, NoKeysException)
+        except FailSendException:
+            except_return(bot, FailSendException)
+        except DisableEndpointException:
+            except_return(bot, DisableEndpointException)
+        except ProblemEndpointException:
+            except_return(bot, ProblemEndpointException)
+        except ProcessingProblemException:
+            except_return(bot, ProcessingProblemException)
+        except SurpriseStatusException:
+            except_return(bot, SurpriseStatusException)
+        except Exception:
+            except_return(bot, Exception)
 
 
 if __name__ == '__main__':
